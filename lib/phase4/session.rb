170c6a700a26e928108658c1194bf3202f183fcd
hyungccho <student@appacademy.io>
2015-09-22 16:28:08 -0700

    add params functionality

--- lib/phase4/session.rb
+++ lib/phase4/session.rb
@@ -6,17 +6,27 @@ module Phase4
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      @cookie_value = request_cookie(req) || {}
+    end
+
+    def request_cookie(req)
+      cookie = req.cookies.find { |kookie| kookie.name == '_rails_lite_app' }
+      JSON.parse(cookie.value) if cookie
     end
 
     def [](key)
+      @cookie_value[key]
     end
 
     def []=(key, val)
+      @cookie_value[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      cookie = WEBrick::Cookie.new('_rails_lite_app', @cookie_value.to_json)
+      res.cookies << cookie
     end
   end
 end
--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,9 +10,28 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = {}
+      @params.merge!(parse_www_encoded_form(req.query_string)) if req.query_string
+      @params.merge!(parse_www_encoded_form(req.body)) if req.body
+      @params.merge!(route_params)
+    end
+
+    def merge_request_body(req)
+      if req.query_string
+        query_hash = parse_www_encoded_form(req.query_body)
+        @params.merge!(query_hash)
+      end
+    end
+
+    def merge_query_string(req)
+      if req.query_string
+        query_hash = parse_www_encoded_form(req.query_string)
+        @params.merge!(query_hash)
+      end
     end
 
     def [](key)
+      @params[key.to_s] || @params[key.to_sym]
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -29,11 +48,39 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      p_hash = {}
+
+      kv_pairs = URI::decode_www_form(www_encoded_form)
+
+      kv_pairs.each do |kv_pair|
+        keys = parse_key(kv_pair[0])
+        val = kv_pair[1]
+        current_hash = p_hash
+
+        keys.each_with_index do |k, i|
+
+          if i == keys.length - 1
+            current_hash[k] = val
+          else
+            current_hash[k] ||= {}
+            current_hash = current_hash[k]
+          end
+
+        end
+      end
+      p p_hash
+      p_hash
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      if key.include?("[")
+        key.split(/\]\[|\[|\]/)
+      else
+        [key]
+      end
     end
+
   end
 end

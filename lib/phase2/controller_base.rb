6d12be5bf0e84570f0c6970de29c7b142cbf1bd3
hyungccho <student@appacademy.io>
2015-09-22 12:59:55 -0700

    Implement Base Controller

--- lib/phase2/controller_base.rb
+++ lib/phase2/controller_base.rb
@@ -4,20 +4,38 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
+      @already_built_response = false
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      if @already_built_response
+        raise("Cannot render twice.")
+      else
+        @already_built_response = true
+        @res.status = 302
+        @res.header["location"] = url
+      end
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, content_type)
+      if @already_built_response
+        raise("Cannot render twice.")
+      else
+        @res.content_type = content_type
+        @res.body = content
+        @already_built_response = true
+      end
     end
   end
 end
